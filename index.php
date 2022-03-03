1a8339308a1cb128c8f8d9ac35384064029feec6
LukaPrcic12345 <bobsagott17@gmail.com>
2022-03-03 15:30:29 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--    Links to css and bootstrap files-->
+    <link rel="stylesheet" href="CSS/staajl.css">
+    <link rel="stylesheet" href="CSS/font-awesome.css">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <title>LP Budgeting</title>
+</head>
+<body>
+
+<!-- main wrapper start-->
+<div class="main-wrapper">
+<!--    header start-->
+    <div class="header">
+        <div class="container">
+            <div class="header-main">
+                <a href="index.php"><span>tws</span>tudy</a>
+            </div>
+            <button type="button" class="header-hamburger-btn js-header-menu-toggler" ></button>
+            <span></span>
+            <button type="button" class="header-close-btn js-header-menu-toggler" >
+                <i class="fas fa-times"></i>
+            </button>
+                <span class="header-backdrop js-header-backdrop"></span>
+                <nav class="header-menu js-header-menu" >
+                    <ul class="menu">
+                        <li class="menu-item"><a href="index.php">home</a></li>
+                        <li class="menu-item"><a href=""></a>courses</li>
+                        <li class="menu-item"><a href=""></a>pages</li>
+                        <li class="menu-item"><a href="contact.php"></a>contact</li>
+                    </ul>
+                </nav>
+            </button>
+        </div>
+    </div>
+
+<!--    header end-->
+</div>
+<!-- main wrapper end-->
+
+
+
+
+<!--Links to JS files-->
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/main.js" ></script>
+
+</body>
+</html>
