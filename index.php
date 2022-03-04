33ec7d697a98118d7d7375b98e7f1578f317b567
LukaPrcic12345 <bobsagott17@gmail.com>
2022-03-04 18:11:52 +0100

    First commit started front end finished showcase part

--- index.php
+++ index.php
@@ -5,50 +5,94 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!--    Links to css and bootstrap files-->
-    <link rel="stylesheet" href="CSS/staajl.css">
-    <link rel="stylesheet" href="CSS/font-awesome.css">
-    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <!--Bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/style.css">
+
     <title>LP Budgeting</title>
 </head>
 <body>
+<!--HEADER START-->
+<!--navbar start-->
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+    <div class="container">
+        <a href="#" class="navbar-brand">LP Budgeting</a>
 
-<!-- main wrapper start-->
-<div class="main-wrapper">
-<!--    header start-->
-    <div class="header">
-        <div class="container">
-            <div class="header-main">
-                <a href="index.php"><span>tws</span>tudy</a>
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navmenu"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="#learn" class="nav-link">What you will learn</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#questions" class="nav-link">Questions</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#instructors" class="nav-link">Instructors</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--navbar end-->
+<!--HEADER END-->
+
+<!--BODY START-->
+<!--Showcase start-->
+<section class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+    <div class="container">
+        <div class="d-sm-flex align-items-center justify-content-between">
+            <div>
+                <h1>Become a <span class="text-warning"> Web Developer</span></h1>
+                <p class="lead my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur culpa
+                    cum delectus deleniti dicta eaque earum, incidunt neque nesciunt numquam optio quae quidem quod
+                    sapiente, similique ullam,
+                    veritatis vero voluptatem?
+                </p>
+                <button class="btn btn-primary btn-lg">Start The Enrollment</button>
             </div>
-            <button type="button" class="header-hamburger-btn js-header-menu-toggler" ></button>
-            <span></span>
-            <button type="button" class="header-close-btn js-header-menu-toggler" >
-                <i class="fas fa-times"></i>
-            </button>
-                <span class="header-backdrop js-header-backdrop"></span>
-                <nav class="header-menu js-header-menu" >
-                    <ul class="menu">
-                        <li class="menu-item"><a href="index.php">home</a></li>
-                        <li class="menu-item"><a href=""></a>courses</li>
-                        <li class="menu-item"><a href=""></a>pages</li>
-                        <li class="menu-item"><a href="contact.php"></a>contact</li>
-                    </ul>
-                </nav>
-            </button>
+            <img class="img-fluid w-50 d-none d-sm-block" src="img/placeholder.jpg" alt="placeholder">
         </div>
     </div>
 
-<!--    header end-->
-</div>
-<!-- main wrapper end-->
+</section>
+<!--Showcase end-->
+<!--Newsletter start-->
+<section class="bg-primary text-light p-5">
+    <div class="container">
+        <div class="d-md-flex justify-content-between align-items-center">
+            <h3 class="mb-3 mb-md-0">Sign up for Our Newsletter</h3>
+
+            <div class="input-group">
+                <input
+                        type="text"
+                        class="form-control news-input"
+                        placeholder="Enter Email"
+                        >
+                <button
+                        class="btn btn-dark btn-lg"
+                        type="button"
+                       >Button</button>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--Newsletter end-->
+<!--BODY END-->
 
 
-
-
-<!--Links to JS files-->
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/main.js" ></script>
-
+<!--Javascript links -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 </body>
 </html>
