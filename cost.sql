fc2c5d3f909c4430b9aea281cadb414aa4fd7c06
LukaPrcic12345 <bobsagott17@gmail.com>
2022-03-29 21:09:14 +0200

    database sql file added

---
+++ cost.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2022 at 09:08 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cost`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `users_id` int(11) NOT NULL,
+  `users_pwd` longtext NOT NULL,
+  `users_email` varchar(100) NOT NULL,
+  `full_name` varchar(200) NOT NULL,
+  `verify_status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '0 = not verified, 1 = verified ',
+  `verify_token` varchar(200) NOT NULL,
+  `ip_adresa` varchar(200) NOT NULL,
+  `web_browser` varchar(200) NOT NULL,
+  `signup_time` datetime NOT NULL,
+  `is_household_admin` tinyint(2) NOT NULL COMMENT '0=not admin, 1=admin',
+  `domacinstvo_id` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`users_id`, `users_pwd`, `users_email`, `full_name`, `verify_status`, `verify_token`, `ip_adresa`, `web_browser`, `signup_time`, `is_household_admin`, `domacinstvo_id`) VALUES
+(76, '$2y$10$GnEHtt7WUmApv1NxiHw7PuNa8hX0Y5TU/FW6jTQeVp5CQ6yghfEEG', 'bobomejl123@gmail.com', 'bobo ime ', 1, 'd6b642b4fc5247a25dd81a28f5c03845', '', '', '0000-00-00 00:00:00', 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_fname` varchar(200) NOT NULL,
+  `admin_email` varchar(200) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
+  `admin_verify_status` tinyint(2) NOT NULL COMMENT '0 = not verified, 1 = verified',
+  `verify_token` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cash_flow`
+--
+
+CREATE TABLE `cash_flow` (
+  `amount_id` int(11) NOT NULL,
+  `amount` int(255) DEFAULT NULL,
+  `users_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cateogries`
+--
+
+CREATE TABLE `cateogries` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `household_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goals`
+--
+
+CREATE TABLE `goals` (
+  `goal_id` int(11) NOT NULL,
+  `goal_name` varchar(255) NOT NULL,
+  `goal_price` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `household`
+--
+
+CREATE TABLE `household` (
+  `household_id` int(11) NOT NULL,
+  `household_name` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`users_id`),
+  ADD UNIQUE KEY `users_email` (`users_email`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `cash_flow`
+--
+ALTER TABLE `cash_flow`
+  ADD KEY `users_id` (`users_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `cateogries`
+--
+ALTER TABLE `cateogries`
+  ADD PRIMARY KEY (`category_id`),
+  ADD KEY `household_id` (`household_id`);
+
+--
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `household`
+--
+ALTER TABLE `household`
+  ADD PRIMARY KEY (`household_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cash_flow`
+--
+ALTER TABLE `cash_flow`
+  ADD CONSTRAINT `cash_flow_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `accounts` (`users_id`),
+  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `cateogries` (`category_id`);
+
+--
+-- Constraints for table `cateogries`
+--
+ALTER TABLE `cateogries`
+  ADD CONSTRAINT `cateogries_ibfk_1` FOREIGN KEY (`household_id`) REFERENCES `household` (`household_id`);
+
+--
+-- Constraints for table `goals`
+--
+ALTER TABLE `goals`
+  ADD CONSTRAINT `goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`users_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
