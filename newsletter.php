c40d27e24175a5258f41516b785486a96ab6d6b8
LukaPrcic12345 <bobsagott17@gmail.com>
2022-04-05 17:56:35 +0200

    design changes

--- newsletter.php
+++ newsletter.php
@@ -1,5 +1,5 @@
 <!--Newsletter start-->
-<section class="bg-primary text-light p-5" style="background: url('./img/okean2.jpg'); background-size: cover; height: 30vh">
+<section class="bg-primary text-light p-5" style="background: url('./img/okean3.jpg'); background-size: cover; height: 20vh">
 <!--<section class="bg-primary text-light p-5" >-->
     <div class="container">
         <div class="d-md-flex justify-content-between align-items-center">
