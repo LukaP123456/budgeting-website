3a6b2ee729c6800f04f6f09e6ccf10647b677b89
LukaPrcic12345 <bobsagott17@gmail.com>
2022-04-16 12:16:20 +0200

    password reset stuff

--- classes/reset-password.class.php
+++ classes/reset-password.class.php
@@ -8,7 +8,7 @@ require './vendor/autoload.php';
 
 class reset_password extends Dbh{
 
-    function sendemail_verify($full_name, $email, $verify_token)
+    function sendemail_verify($full_name, $email,$url)
     {
 
         $mail = new PHPMailer(true);
@@ -16,8 +16,8 @@ class reset_password extends Dbh{
         $mail->isSMTP();                                                        //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                                         //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                                 //Enable SMTP authentication
-        $mail->Username = 'lpbudgeting@gmail.com';                          //SMTP username
-        $mail->Password = 'supertajnasifra123';                                    //SMTP password
+        $mail->Username = 'lpbudgeting@gmail.com';                              //SMTP username
+        $mail->Password = 'supertajnasifra123';                                 //SMTP password
 
         $mail->SMTPSecure = "tls";                                              //Enable implicit TLS encryption
         $mail->Port = 587;                                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
@@ -28,16 +28,14 @@ class reset_password extends Dbh{
 
         //Content
         $mail->isHTML(true);                                                            //Set email format to HTML
-        $mail->Subject = 'Email verification from LP Budgeting';
-
-        $date_time = date("d-m-Y H:i:s");
+        $mail->Subject = 'Reset your password for LP Budgeting';
 
         $email_template = "
-            <h1>Click the link below to verify your account with LP Budgeting</h1>
-            <h3>Hello $full_name you have registered with LP Budgeting on $date_time with your email account $email</h3>
-            <h4>Verify your email address to Login with the below given link</h4>
-            <br><br>
-            <h1><a href='http://localhost/BUDGETING_WEBSITE/includes/verify_email.php?token=$verify_token'>Click me to verify</a></h1>
+            <h1>We received a password reset request.</h1>
+            <p>The link to reset your password is below if you did not make this request you can ignore this email.</p>
+            <hr>
+            <p>Here is your password reset link</p><br>
+            <h1><a href='$url'>Click me to reset your password</a></h1>
         ";
         $mail->Body = $email_template;
 
@@ -46,17 +44,54 @@ class reset_password extends Dbh{
 
     }
 
-    public function reset_user($email, $password_token, $password_selector, $password_expires){
-        $stmt = $this->connect()->prepare("DELETE FROM password_reset WHERE password_reset_email=?;");
+    public function reset_user_password($email, $password_token, $password_selector, $password_expires){
+        $stmt_check = $this->connect()->prepare( "SELECT * FROM cost.accounts where users_email=? LIMIT 1;");
+        $hashed_token = password_hash($password_token,PASSWORD_DEFAULT);
+
+
+        if ($stmt_check->execute($email))
+        {
+            //We have the entered email in the database now we will delete the old password
+            $user = $stmt_check->fetchAll(PDO::FETCH_ASSOC);
+            $fullname = $user["full_name"];
+
+
+
+            $stmt_delete = $this->connect()->prepare("UPDATE `accounts` SET `users_pwd` = '' WHERE `accounts`.`users_email` =?;");
+            if ($stmt_delete->execute($email))
+            {
+                //We deleted the old password now we have to change it into the new value which we will get from the reset-form.php page and insert password_reset_token into the database
+                $stmt_insert = "INSERT INTO accounts(password_reset_token,password_reset_expires) VALUES (?,?);";
+                if ($stmt_insert->execute($hashed_token,$password_expires))
+                {
+                    //We inserted the reset token into the database and the time it has before it is invalid
+                    $this->sendemail_verify($fullname,$email,$hashed_token);
+
+                }
+
+
+            }
+
+
+        }
+        else
+        {
+            //Error the entered email doesn't exist
+            echo "error";
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        $stmt = $this->connect()->prepare("UPDATE `accounts` SET `users_pwd` = '' and verify_token = '' WHERE `accounts`.`users_email` =?;");
 
         if (!$stmt->execute($email))
         {
             echo "There was an error";
+            exit();
         }
         else
         {
-            $hashed_token = password_hash($password_token,PASSWORD_DEFAULT) ;
-            $insert_stmt = "INSERT INTO password_reset(password_reset_email,password_reset_selector,password_reset_token,password_reset_expires) VALUES (?,?,?,?);";
+
+            $insert_stmt = "INSERT INTO accounts(verify_token,users_pwd,) VALUES (?,?,?,?);";
 
            if (!$insert_stmt->execute($email,$password_selector,$hashed_token,$password_expires))
             {
--- includes/reset-request.php
+++ includes/reset-request.php
@@ -3,11 +3,12 @@
 if (isset($_POST['reset-request-submit']))
 {
     //Selector(token number 1) has the value of a radnom binary number converted to hexadecimal
+    //We use selector to verify that we are selecting the correct user
     $selector = bin2hex( random_bytes(8));
     //token number 2
     $token = random_bytes(32);
 
-    $url = "http://localhost/BUDGETING_WEBSITE/includes/create-new-password.php?selector=" . $selector . "&validator" . bin2hex($token );
+    $url = "http://localhost/BUDGETING_WEBSITE/includes/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token );
 
     //Expiary date for the token
     //date("U") gives us the current date in seconds since 1970
@@ -17,7 +18,7 @@ if (isset($_POST['reset-request-submit']))
     $user_email = $_POST["email"];
 
     $reset = new reset_password();
-    $reset->reset_user($user_email,$selector,$token,$expires);
+    $reset->reset_user_password($user_email,$token,$selector,$expires,$url);
 
 
 }
