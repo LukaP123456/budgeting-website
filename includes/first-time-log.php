cf7c1b006b4d31dfcfb28850cb2b6e3a7f24d69b
LukaPrcic12345laptop <bobsagott17@gmail.com>
2022-05-04 16:45:53 +0200

    started work on loged in user

--- includes/first-time-log.php
+++ includes/first-time-log.php
@@ -7,15 +7,18 @@ if (isset($_POST['submit'])){
 
     if (isset($_POST['alone-box'])){
         $alone_box = 1;
+        $email = "";
     }
     else{
         $alone_box = 0;
+        $email = $_POST['email-friend'];
     }
 
+    $group_name = $_POST['group-name'];
 
-    $email = $_POST['email'];
 
-    echo $email;
+
+
 
 }
 
