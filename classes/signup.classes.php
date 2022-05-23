8be6fb18c3225c1a2b12216ba4265153198b0538
LukaPrcic12345 <bobsagott17@gmail.com>
2022-05-23 07:20:20 +0200

    password change

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -77,7 +77,7 @@ class Signup extends Dbh
         $mail->Host = 'smtp.gmail.com';                                         //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                                 //Enable SMTP authentication
         $mail->Username = 'lpbudgeting456@gmail.com';                              //SMTP username
-        $mail->Password = 'lpbudgetinggunduliceva63';                                 //SMTP password
+        $mail->Password = 'lpbudgetingcankareva11';                                 //SMTP password
 
         $mail->SMTPSecure = "tls";                                              //Enable implicit TLS encryption
         $mail->Port = 587;                                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
