d91842a5aa7460811fe5e12e6b74d7207374eb2c
LukaPrcic12345laptop <bobsagott17@gmail.com>
2022-06-10 06:08:50 +0200

    made pin error handler

--- includes/pin_verify.php
+++ includes/pin_verify.php
@@ -4,17 +4,27 @@ require_once "../classes/insert-get-class.php";
 
 if (isset($_POST['PIN-submit'])){
 
-    $entered_pin = $_POST['PIN'];
-    $pin_length = strlen($entered_pin);
+    if (empty($_POST['PIN'])){
+        header("location:../includes/pin_verification.php?error=no_pin");
+        die();
+    }else{
+        $entered_pin = $_POST['PIN'];
+        $pin_length = strlen($entered_pin);
 
-    if (isset($entered_pin) AND $pin_length <= 6){
-
+        if (isset($entered_pin) AND $pin_length <= 6){
 
             $check = new Insert_get();
 
             $check->check_pin($entered_pin,$_COOKIE['users_id']);
+            die();
 
+        }else{
+            header("location:../includes/pin_verification.php?error=no_pin");
+            die();
+        }
     }
+
+
 
 
 }
